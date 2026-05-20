9a6573a6c46d37707bdef76dec69f33165619796
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2026-05-20 02:37:22 +0100

    Remove unnecessary test for invalid usage

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -706,21 +706,6 @@ class UtilsTest extends TestCase
         self::assertFalse($modified->hasHeader('123'));
     }
 
-    public function testModifyRequestPreservesZeroHeaderNameWithoutParsingListStyleHeader(): void
-    {
-        $request = new Psr7\Request('POST', 'http://example.com', [
-            'Content-type: application/x-www-form-urlencoded',
-        ]);
-
-        $modified = Psr7\Utils::modifyRequest($request, [
-            'set_headers' => ['X-Test' => '1'],
-        ]);
-
-        self::assertSame('Content-type: application/x-www-form-urlencoded', $modified->getHeaderLine('0'));
-        self::assertSame('', $modified->getHeaderLine('Content-Type'));
-        self::assertSame('1', $modified->getHeaderLine('X-Test'));
-    }
-
     public function testModifyServerRequestWithUploadedFiles(): void
     {
         $request = new Psr7\ServerRequest('GET', 'http://example.com/bla');
