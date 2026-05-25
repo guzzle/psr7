5b2afd51a673430e96391326936a71c318a77efb
Graham Campbell <hello@gjcampbell.co.uk>
2026-05-25 22:25:26 +0100

    Order RFC 3986 constants by section

--- src/Rfc3986.php
+++ src/Rfc3986.php
@@ -10,16 +10,16 @@ namespace GuzzleHttp\Psr7;
 final class Rfc3986
 {
     /**
-     * Unreserved characters for use in a regex.
-     *
-     * @see https://datatracker.ietf.org/doc/html/rfc3986#section-2.3
-     */
-    public const CHAR_UNRESERVED = 'a-zA-Z0-9_\-\.~';
-
-    /**
      * Sub-delims for use in a regex.
      *
      * @see https://datatracker.ietf.org/doc/html/rfc3986#section-2.2
      */
     public const CHAR_SUB_DELIMS = '!\$&\'\(\)\*\+,;=';
+
+    /**
+     * Unreserved characters for use in a regex.
+     *
+     * @see https://datatracker.ietf.org/doc/html/rfc3986#section-2.3
+     */
+    public const CHAR_UNRESERVED = 'a-zA-Z0-9_\-\.~';
 }
