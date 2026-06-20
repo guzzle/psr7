fe547df22b8fb3e31af5ac794cb2cf45dc0087df
Graham Campbell <hello@gjcampbell.co.uk>
2026-06-20 23:39:22 +0100

    Report message parser PCRE failures

--- src/Message.php
+++ src/Message.php
@@ -209,7 +209,11 @@ final class Message
 
         $messageParts = preg_split("/\r?\n\r?\n/", $message, 2);
 
-        if ($messageParts === false || count($messageParts) !== 2) {
+        if ($messageParts === false) {
+            throw new \RuntimeException('Unable to split HTTP message: '.preg_last_error_msg());
+        }
+
+        if (count($messageParts) !== 2) {
             throw new \InvalidArgumentException('Invalid message: Missing header delimiter');
         }
 
@@ -217,24 +221,48 @@ final class Message
         $rawHeaders .= "\r\n"; // Put back the delimiter we split previously
         $headerParts = preg_split("/\r?\n/", $rawHeaders, 2);
 
-        if ($headerParts === false || count($headerParts) !== 2) {
+        if ($headerParts === false) {
+            throw new \RuntimeException('Unable to split HTTP message headers: '.preg_last_error_msg());
+        }
+
+        if (count($headerParts) !== 2) {
             throw new \InvalidArgumentException('Invalid message: Missing status line');
         }
 
         [$startLine, $rawHeaders] = $headerParts;
 
-        if (preg_match("/(?:^HTTP\/|^[A-Z]+ \S+ HTTP\/)(\d+(?:\.\d+)?)/i", $startLine, $matches) && $matches[1] === '1.0') {
+        $versionMatch = preg_match("/(?:^HTTP\/|^[A-Z]+ \S+ HTTP\/)(\d+(?:\.\d+)?)/i", $startLine, $matches);
+
+        if ($versionMatch === false) {
+            throw new \RuntimeException('Unable to parse HTTP start line: '.preg_last_error_msg());
+        }
+
+        if ($versionMatch === 1 && $matches[1] === '1.0') {
             // Header folding is deprecated for HTTP/1.1, but allowed in HTTP/1.0
             $rawHeaders = preg_replace(Rfc9112::HEADER_FOLD_REGEX, ' ', $rawHeaders);
+
+            if ($rawHeaders === null) {
+                throw new \RuntimeException('Unable to unfold HTTP headers: '.preg_last_error_msg());
+            }
         }
 
         /** @var array[] $headerLines */
         $count = preg_match_all(Rfc9112::HEADER_REGEX, $rawHeaders, $headerLines, PREG_SET_ORDER);
 
+        if ($count === false) {
+            throw new \RuntimeException('Unable to parse HTTP headers: '.preg_last_error_msg());
+        }
+
         // If these aren't the same, then one line didn't match and there's an invalid header.
         if ($count !== substr_count($rawHeaders, "\n")) {
             // Folding is deprecated, see https://datatracker.ietf.org/doc/html/rfc9112#section-5.2
-            if (preg_match(Rfc9112::HEADER_FOLD_REGEX, $rawHeaders)) {
+            $hasFoldedHeader = preg_match(Rfc9112::HEADER_FOLD_REGEX, $rawHeaders);
+
+            if ($hasFoldedHeader === false) {
+                throw new \RuntimeException('Unable to inspect HTTP header folding: '.preg_last_error_msg());
+            }
+
+            if ($hasFoldedHeader === 1) {
                 throw new \InvalidArgumentException('Invalid header syntax: Obsolete line folding');
             }
 
@@ -365,7 +393,13 @@ final class Message
     {
         $data = self::parseMessage($message);
         $matches = [];
-        if (!preg_match('/^(?P<method>[!#$%&\'*+.^_`|~0-9A-Za-z-]+) (?P<target>[^\x00-\x20\x7F]+) HTTP\/(?P<version>\d+(?:\.\d+)?)$/D', $data['start-line'], $matches)) {
+        $matched = preg_match('/^(?P<method>[!#$%&\'*+.^_`|~0-9A-Za-z-]+) (?P<target>[^\x00-\x20\x7F]+) HTTP\/(?P<version>\d+(?:\.\d+)?)$/D', $data['start-line'], $matches);
+
+        if ($matched === false) {
+            throw new \RuntimeException('Unable to parse request start line: '.preg_last_error_msg());
+        }
+
+        if ($matched === 0) {
             throw new \InvalidArgumentException('Invalid request string');
         }
 
@@ -449,7 +483,13 @@ final class Message
         // According to https://datatracker.ietf.org/doc/html/rfc9112#section-4
         // the space between status-code and reason-phrase is required. But
         // browsers accept responses without space and reason as well.
-        if (!preg_match('/^HTTP\/(?P<version>\d+(?:\.\d+)?) (?P<status>[1-5][0-9]{2})(?: (?P<reason>[\x09\x20-\x7E\x80-\xFF]*))?$/D', $data['start-line'], $matches)) {
+        $matched = preg_match('/^HTTP\/(?P<version>\d+(?:\.\d+)?) (?P<status>[1-5][0-9]{2})(?: (?P<reason>[\x09\x20-\x7E\x80-\xFF]*))?$/D', $data['start-line'], $matches);
+
+        if ($matched === false) {
+            throw new \RuntimeException('Unable to parse response start line: '.preg_last_error_msg());
+        }
+
+        if ($matched === 0) {
             throw new \InvalidArgumentException('Invalid response string: '.$data['start-line']);
         }
 
