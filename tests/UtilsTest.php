569440705434763f266ec2b62ace7a641e9388f0
Graham Campbell <hello@gjcampbell.co.uk>
2026-07-07 19:58:52 +0100

    Include non-default URI ports in modifyRequest Host headers

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -1009,6 +1009,26 @@ class UtilsTest extends TestCase
         self::assertSame('www.foo.com:8000', (string) $r2->getHeaderLine('host'));
     }
 
+    public function testCanModifyRequestWithUriAndZeroPort(): void
+    {
+        $r1 = new Psr7\Request('GET', 'http://foo.com');
+        $r2 = Psr7\Utils::modifyRequest($r1, [
+            'uri' => (new Psr7\Uri('http://www.foo.com'))->withPort(0),
+        ]);
+        self::assertSame('http://www.foo.com:0', (string) $r2->getUri());
+        self::assertSame('www.foo.com:0', (string) $r2->getHeaderLine('host'));
+    }
+
+    public function testCanModifyRequestWithUriAndNonHttpSchemePort(): void
+    {
+        $r1 = new Psr7\Request('GET', 'http://foo.com');
+        $r2 = Psr7\Utils::modifyRequest($r1, [
+            'uri' => new Psr7\Uri('ws://www.foo.com:8080'),
+        ]);
+        self::assertSame('ws://www.foo.com:8080', (string) $r2->getUri());
+        self::assertSame('www.foo.com:8080', (string) $r2->getHeaderLine('host'));
+    }
+
     public function testModifyRequestRejectsInvalidUriHostFromCustomUri(): void
     {
         $uri = $this->createMock(UriInterface::class);
@@ -1018,6 +1038,18 @@ class UtilsTest extends TestCase
         $this->expectException(\InvalidArgumentException::class);
 
         Psr7\Utils::modifyRequest(new Psr7\Request('GET', '/'), ['uri' => $uri]);
+    }
+
+    public function testModifyRequestOmitsDefaultPortFromCustomUri(): void
+    {
+        $uri = $this->createMock(UriInterface::class);
+        $uri->method('getHost')->willReturn('www.foo.com');
+        $uri->method('getPort')->willReturn(80);
+        $uri->method('getScheme')->willReturn('http');
+
+        $r2 = Psr7\Utils::modifyRequest(new Psr7\Request('GET', '/'), ['uri' => $uri]);
+
+        self::assertSame('www.foo.com', $r2->getHeaderLine('host'));
     }
 
     public function testCanModifyRequestWithFalseyUriHost(): void
