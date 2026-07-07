4a79802d121a1d45707fea8d39d623f5903d0dc1
Graham Campbell <hello@gjcampbell.co.uk>
2026-07-07 14:19:57 +0100

    Fix anonymous class instantiation style

--- tests/UriTest.php
+++ tests/UriTest.php
@@ -700,7 +700,7 @@ class UriTest extends TestCase
 
         // The split must not validate unrelated components: this host is
         // accepted by the withers but rejected by the parser.
-        $plainSubclassUri = (new class() extends Uri {
+        $plainSubclassUri = (new class extends Uri {
         })->withScheme('http')->withHost('[v1.fe80::a+en1]')->withPath('/');
 
         self::assertSame('/', Uri::rawPath($plainSubclassUri));
